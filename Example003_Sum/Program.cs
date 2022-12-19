ab40f09468738927a94fa85d0543a0001154b826
smira12 <m1ronsa89@gmail.com>
2022-12-19 11:13:49 +0300

    Add modified Example003 Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -2,3 +2,10 @@
 int numberB = 5;
 int result = numberA + numberB;
 Console.WriteLine(result);
+
+int numberC = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+int numberD = new Random().Next(1, 10);
+Console.WriteLine(numberD);
+int res = numberC + numberD;
+Console.WriteLine(res);
